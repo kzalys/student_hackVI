6b8fc5ce3a939a0082ceff2a3650ced017655596
Kristijonas <kzalys@gmail.com>
2018-03-19 20:33:03 +0000

    Reverted text scalling changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+// Timer
+
+var countDownDate = new Date("Apr 26, 2018 09:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (hours < 10)
+        hours = "0" + hours;
+    if (days < 10)
+        days = "0" + days;
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    if (seconds < 10)
+        seconds = "0" + seconds;
+
+    document.getElementById("daysNumber").innerHTML = days;
+    document.getElementById("hoursNumber").innerHTML = hours;
+    document.getElementById("minutesNumber").innerHTML = minutes;
+    document.getElementById("secondsNumber").innerHTML = seconds;
+
+}, 1000);
+
+// Map
+
+function initMap() {
+    var venue = { lat: 53.470350850357946, lng: -2.240422239629993 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: venue
+    });
+    var marker = new google.maps.Marker({
+        position: venue,
+        map: map
+    });
+}
+
+// Parallax
+
 // $(document).ready(function () {
 //     // resize();
 // });
@@ -28,40 +71,3 @@
 //     console.log($(window).height());
 //     parallax();
 // }
-
-var countDownDate = new Date("Apr 26, 2018 09:00:00").getTime();
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    if (hours < 10)
-        hours = "0" + hours;
-    if (days < 10)
-        days = "0" + days;
-    if (minutes < 10)
-        minutes = "0" + minutes;
-    if (seconds < 10)
-        seconds = "0" + seconds;
-
-    document.getElementById("daysNumber").innerHTML = days;
-    document.getElementById("hoursNumber").innerHTML = hours;
-    document.getElementById("minutesNumber").innerHTML = minutes;
-    document.getElementById("secondsNumber").innerHTML = seconds;
-
-}, 1000);
-
-function initMap() {
-    var venue = { lat: 53.470350850357946, lng: -2.240422239629993 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: venue
-    });
-    var marker = new google.maps.Marker({
-        position: venue,
-        map: map
-    });
-}
